6b65c18aad86718afb8ea1f0031a5ff968a39dd3
Kshitij Banerjee <ksh.dce@gmail.com>
2023-07-15 12:47:30 +0800

    add book summary for grit

--- copy_assets.sh
+++ copy_assets.sh
@@ -1,2 +1,2 @@
-git status  | grep md | awk -F' ' '{ print $2 }' | xargs cat | grep png | awk -F'[()]' '{print $2}' | awk -F'/' '{print $NF}' | while read -r filename; do cp "/Users/kay/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/assets/$filename" "./static"; done  
+git s| grep md | awk -F' ' '{ print $2 }' | xargs cat | grep png | awk -F'[()]' '{print $2}' | awk -F'/' '{print $NF}' | while read -r filename; do cp "/Users/kay/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/assets/$filename" "./static"; done  
 echo "copy images done"
